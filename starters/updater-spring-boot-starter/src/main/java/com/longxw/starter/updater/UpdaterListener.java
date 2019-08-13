d5e779d1ed8ed775f25382ce508b8e534182420f
longxw <longxw@minstone.com.cn>
2019-08-13 10:11:16 +0800

    sql版本控制器

--- starters/updater-spring-boot-starter/src/main/java/com/longxw/starter/updater/UpdaterListener.java
+++ starters/updater-spring-boot-starter/src/main/java/com/longxw/starter/updater/UpdaterListener.java
@@ -11,61 +11,71 @@ import org.springframework.util.ResourceUtils;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class UpdaterListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private String defaultPath = "classpath:script/sql";
+
     @Autowired
     UpdaterDataSourceProperties updaterDataSourceProperties;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         ApplicationContext applicationContext = event.getApplicationContext();
-        doUpdater(applicationContext);
+        try {
+            doUpdater(applicationContext);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
-    private void doUpdater(ApplicationContext applicationContext){
-        Connection connection = null;
-        try {
-            connection = getConnection(applicationContext);
-            DbTool dbTool = new DbTool(connection);
-            String version = (String)dbTool.executeQuery("select v_updater from updater_version").getObject(1);
-            Map<String,String> map = getScript(version);
-            String lastVersion = null;
-            for(String key : map.keySet()){
-                String[] sqls = map.get(key).split(";");
-                for(String sql : sqls){
-                    try {
-                        dbTool.executeUpdate(sql);
-                        lastVersion = key;
-                    }catch (Exception e){
-
-                    }
-
-                }
-            }
-            if(lastVersion == null){
-                return;
-            }
-            dbTool.executeUpdate("update updater_version set version = ?",new String[]{lastVersion});//更新版本信息
-        }catch (SQLException e){
-
-        }finally {
-            if(connection != null){
-                try {
-                    connection.close();
-                }catch (SQLException e){
-
-                }
-
+    private void doUpdater(ApplicationContext applicationContext) throws SQLException,IOException{
+        Connection connection = getConnection(applicationContext);
+        DbTool dbTool = new DbTool(connection);
+        String version = getCurrentVersion(dbTool);
+        Map<String,String> map = getScript(version);
+        String lastVersion = null;
+        for(String key : map.keySet()){
+            String[] sqls = map.get(key).split(";");
+            for(String sql : sqls){
+                dbTool.executeUpdate(sql);
+                lastVersion = key;
             }
         }
+        if(lastVersion == null){
+            return;
+        }
+        dbTool.executeUpdate("update updater_version set version = ?",new String[]{lastVersion});//更新版本信息
+        if(connection != null){
+            connection.close();
+        }
+
     }
 
-    /** 获取符合条件的 sql
+    /**获取数据库中当前版本号
+     * @author longxw
+     * @since 2019-8-13
+     */
+    private String getCurrentVersion(DbTool dbTool) throws IOException,SQLException{
+        String sql = FileTool.readText(Paths.get(defaultPath,"init_updater.sql"));
+        dbTool.executeUpdate(sql);
+        ResultSet resultSet = dbTool.executeQuery("select v_updater from updater_version");
+        String version = "v1.0.0";
+        if(resultSet.next()){
+            version = (String)resultSet.getObject(1);
+        }
+        return version;
+    }
+
+    /** 获取符合条件的 sql, map 的 key=version，value=scripts
      * @author longxw
      * @since 2019-8-12
      */
@@ -79,7 +89,18 @@ public class UpdaterListener implements ApplicationListener<ContextRefreshedEven
                     .collect(Collectors.toList());
             Map<String,String> map = new TreeMap();
             fileList.forEach( file -> {
-                map.put(file.getName(),FileTool.readText(file));
+                StringBuffer sb =new StringBuffer();
+                try{
+                    FileTool.readLines(file).forEach(line -> {
+                        if( !line.startsWith("#")){
+                            sb.append(line);
+                        }
+                    });
+                    map.put(file.getName(),sb.toString());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
             });
             return map;
         }catch (Exception e){
--- starters/updater-spring-boot-starter/src/main/java/com/longxw/starter/updater/tool/FileTool.java
+++ starters/updater-spring-boot-starter/src/main/java/com/longxw/starter/updater/tool/FileTool.java
@@ -4,16 +4,32 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.List;
 
 public class FileTool {
 
-    public static String readText(File file) {
-        try{
-            return new String(readBytes(file), "UTF-8");
-        }catch (IOException e){
-            return "";
-        }
+    private static Charset defaultCharset = Charset.forName("UTF-8");
+
+    public static List<String> readLines(File file) throws IOException{
+        return readLines(file.toPath());
+    }
+
+    public static List<String> readLines(Path path) throws IOException{
+        return Files.readAllLines(path,defaultCharset);
+    }
+
+    public static String readText(Path path) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        readLines(path).forEach( line -> sb.append(line));
+        return sb.toString();
+    }
+
+    public static String readText(File file) throws IOException{
+        return new String(readBytes(file), defaultCharset);
     }
 
     public static byte[] readBytes(File file) throws IOException {
