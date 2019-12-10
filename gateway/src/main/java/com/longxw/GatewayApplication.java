a3a1c9c0dd0e7903d4b19881267a1b0151154136
longxw <longxw@minstone.com.cn>
2019-12-10 11:05:03 +0800

    添加nacos依赖

--- gateway/src/main/java/com/longxw/GatewayApplication.java
+++ gateway/src/main/java/com/longxw/GatewayApplication.java
@@ -1,8 +1,22 @@
 package com.longxw;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+/**
+ * @author longxw
+ * @since 2019/12/10
+ */
+@Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
@@ -10,4 +24,22 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class,args);
     }
 
+    @Bean
+    public GlobalFilter customFilter() {
+        return new CustomGlobalFilter();
+    }
+
+    public class CustomGlobalFilter implements GlobalFilter, Ordered {
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            log.info("custom global filter");
+            return chain.filter(exchange);
+        }
+
+        @Override
+        public int getOrder() {
+            return -1;
+        }
+    }
 }
